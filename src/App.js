35ff896b531ac103b014efe49e204117810cff64
Ronny230797 <ronny230797@gmail.com>
2018-10-30 18:19:41 -0600

    Despiches de kenneth

--- src/App.js
+++ src/App.js
@@ -13,21 +13,12 @@ class App extends Component {
     const { children } = this.props;
     return (
       <body>
-<<<<<<< HEAD
          <Navigation />
         <div className="container-fluid" id="div_ancho_completo">
          
           <Contenedor body={children} />
         </div>
        
-=======
-        <div >
-          <Navigation />
-        </div>
-        <div className="container" id="div_ancho_completo">
-          <Contenedor body={children} />
-        </div>
->>>>>>> 34f02720ffe4e1a739b4f20815ce1462e89b6042
         <Footer />
       </body>
     );
