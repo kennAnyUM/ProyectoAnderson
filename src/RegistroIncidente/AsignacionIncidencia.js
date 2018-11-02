9c05739a57fb122177370cf0e24efd4a3845d2f5
Ronny230797 <ronny230797@gmail.com>
2018-11-02 15:59:52 -0600

    Texto incidencia

--- src/RegistroIncidente/AsignacionIncidencia.js
+++ src/RegistroIncidente/AsignacionIncidencia.js
@@ -19,6 +19,7 @@ class AsignacionIncidencia extends React.Component {
               <h3 className="mt-4">Asignar Incidencia</h3>
             </header>
             <div>
+              {ShowIncident()}
               {FilterUser()}
               <ColaboradorTabla />
               <div class="pagination justify-content-end">
@@ -46,7 +47,7 @@ class ColaboradorTabla extends React.Component {
     const colaboradorCard = this.state.colaboradores.map((colaborador) => {
       return (
         <tr>
-          <td><input type="checkbox" value=""/></td>
+          <td><input type="checkbox" value="" /></td>
           <th scope="row">{colaborador.area}</th>
           <td>{colaborador.primernombre}</td>
           <td>{colaborador.primerapellido}</td>
@@ -59,33 +60,33 @@ class ColaboradorTabla extends React.Component {
     return (
 
       <div className="container">
-       <div className="row">
-        <div className="col-xs-12 col-md-12">
-          <div className="Container-div">
-          </div>
+        <div className="row">
+          <div className="col-xs-12 col-md-12">
+            <div className="Container-div">
+            </div>
 
-      <table id="tableColab" class="table table-bordered table-light table-striped mt-4">
-         <thead className="table-dark" >
-          <tr >
-            <th>Asignar
+            <table id="tableColab" class="table table-bordered table-light table-striped mt-4">
+              <thead className="table-dark" >
+                <tr >
+                  <th>Asignar
             </th>
-            <th>Area Especializacion
+                  <th>Area Especializacion
             </th>
-            <th >Primer Nombre
+                  <th >Primer Nombre
             </th>
-            <th >Primer Apellido
+                  <th >Primer Apellido
             </th>
-            <th >Correo Electronico
+                  <th >Correo Electronico
             </th>
-          </tr>
-        </thead>
-        <tbody>
-          {colaboradorCard}
-        </tbody>
-      </table>
+                </tr>
+              </thead>
+              <tbody>
+                {colaboradorCard}
+              </tbody>
+            </table>
 
           </div>
-       </div>
+        </div>
       </div>
 
     );
@@ -107,11 +108,41 @@ function FilterUser() {
 
               <label for="exampleFormControl">Filtrar Incidencia</label>
               <br></br>
-              <label class="checkbox-inline"> <input type="checkbox" value=""/>Bases de Datos&nbsp;&nbsp;</label>
-              <label class="checkbox-inline"> <input type="checkbox" value=""/>Redes&nbsp;&nbsp;</label>
-              <label class="checkbox-inline"> <input type="checkbox" value=""/>Desarrollo&nbsp;&nbsp;</label>
-              <label class="checkbox-inline"> <input type="checkbox" value=""/>Infraestructura&nbsp;&nbsp;</label>
+              <label class="checkbox-inline"> <input type="checkbox" value="" />Bases de Datos&nbsp;&nbsp;</label>
+              <label class="checkbox-inline"> <input type="checkbox" value="" />Redes&nbsp;&nbsp;</label>
+              <label class="checkbox-inline"> <input type="checkbox" value="" />Desarrollo&nbsp;&nbsp;</label>
+              <label class="checkbox-inline"> <input type="checkbox" value="" />Infraestructura&nbsp;&nbsp;</label>
 
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+
+  );
+}
+
+function ShowIncident() {
+  return (
+
+    <div className="container">
+
+      <div className="row">
+
+        <div className="col-xs-12 col-md-12">
+
+          <div className="Container-div">
+
+            <div >
+              <h1>Incidencia</h1>
+              <blockquote>
+                <p>Se presento una incidencia en el area de base de datos, en el servidor externo de la empresa, se debe recurrir a restablecer todos los dominios, para entrar nuevamente al trabajo normal.</p>
+              </blockquote>
             </div>
 
           </div>
