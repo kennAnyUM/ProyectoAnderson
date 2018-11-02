d8019d9edcb6c359b1914efc88ff8e146368c2d0
Cesar Jimenez <kenneth.uglade@ucrso.info>
2018-11-02 14:52:46 -0600

    algunos cambios

--- src/Rutas/AppRutas.js
+++ src/Rutas/AppRutas.js
@@ -9,6 +9,7 @@ import Form from '../RegistroIncidente/Form';
 import Configuracion from '../configuracion/Perfil';
 import GetAdmin from '../Administrator/GetAdmin';
 import BlockUser from '../Administrator/Block_User';
+import sidebar from '../components/SidebarAdmin';
 
 
 const AppRutas = () =>
@@ -20,6 +21,7 @@ const AppRutas = () =>
       <Route exact path="/AjustePerfil" component={Configuracion} />
       <Route exact path="/getAdmin" component={GetAdmin} />
       <Route exact path="/BlockUser" component={BlockUser} />
+      <Route exact path="/sidebar" component={sidebar} />
     </Switch>
 
   </App>;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,7 +9,7 @@ class navigatiom extends React.Component {
   render() {
     return (
       <nav className="container-fluid navbar navbar-expand-lg navbar-light" id="nav">
-      <a className="navbar-brand" href="#"><img src={require("./imagenesImpesa/ICONO ANDERSON1.png")} width="35" height="35"></img></a>
+        <a className="navbar-brand" href="#"><img src={require("./imagenesImpesa/ICONO ANDERSON1.png")} width="35" height="35"></img></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -17,7 +17,7 @@ class navigatiom extends React.Component {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-            <Link to="/Incidentes">    <a className="nav-link" href="#">Inicio<span className="sr-only">(current)</span></a></Link>
+              <Link to="/Incidentes">    <a className="nav-link" href="#">Inicio<span className="sr-only">(current)</span></a></Link>
             </li>
             <li className="nav-item">
               <Link to="/InsertarIncidencia"><a className="nav-link" >Crear Incidencia<span className="sr-only">(current)</span></a></Link>
@@ -25,13 +25,14 @@ class navigatiom extends React.Component {
 
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Configuracion</a>
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administrar</a>
 
               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
+              <Link to="/BlockUser">  <a className="dropdown-item" href="#">Bloquear Usuarios</a></Link>
                 <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="#">Something else here</a>
+                <a className="dropdown-item" href="#">Tecnologías</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Areas</a>
               </div>
             </li>
             <li className="nav-item">
@@ -40,8 +41,8 @@ class navigatiom extends React.Component {
           </ul>
 
           <form className="form-inline my-2 my-lg-0 dropdown">
-        
-         
+
+
 
             <button className="btn btn-outline-primary my-2 my-sm-0 dropdown-toggle" role="button"
               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><PersonIcon /> Maria Jose Brenes</button>
