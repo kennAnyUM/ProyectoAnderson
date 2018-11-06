e8ee29be5b3fb669c85a147f05f1e5b66b6b25ae
Ronny230797 <ronny230797@gmail.com>
2018-11-06 17:37:23 -0600

    Base

--- src/RegistroIncidente/InformacionIncidencia.js
+++ src/RegistroIncidente/InformacionIncidencia.js
@@ -3,63 +3,118 @@ import Navigation from '../components/Navigation';
 
 class InformacionIncidencia extends React.Component {
     constructor(props) {
-      super();
+        super();
     }
     render() {
-      return (
-        <div className="container-fluid">
-          <Navigation />
-          <form className="container" >
-  
-            <fieldset className="fields">
-              <header className="App-header">
-                <br /><br /><br />
-                <h3 className="mt-4">Informacion Incidencia</h3>
-              </header>
-              <div>
-                {BodyInformation()}
-                <div class="pagination justify-content-end">
-                  <button class="btn btn-light  " type="submit">Cancelar</button>
-                  <button class="btn btn-primary" type="submit">Notificar</button>
-                </div>
-              </div>
-            </fieldset>
-          </form>
-  
-        </div>
-      )
-    }
-  }
+        return (
+            <div className="container-fluid">
+                <Navigation />
+                <form className="container" >
 
+                    <fieldset className="fields">
+                        <header className="App-header">
+                            <br /><br /><br />
+                            <h3 className="mt-4">Informacion Incidencia</h3>
+                        </header>
+                        <div>
+                            <br></br>
+                            {BodyInformation()}
+                            <br></br>
+                            <br></br>
+                            <div class="pagination justify-content-end">
+                                <button class="btn btn-light  " type="submit">Cancelar</button>
+                                <button class="btn btn-primary" type="submit">Notificar</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
 
-  function BodyInformation() {
-    return (
-  
-      <div className="container">
-  
-        <div className="row">
-  
-          <div className="col-xs-12 col-md-12">
-  
-            <div className="Container-div">
-  
-              <div className="form-group blue-border-focus">
-                <label for="exampleFormControlTextarea5">Inserte la descripción de la incidencia</label>
-                <textarea className="form-control" id="exampleFormControlTextarea5" rows="3"></textarea>
-              </div>
-  
             </div>
-  
-          </div>
-  
+        )
+    }
+}
+
+
+function BodyInformation() {
+    return (
+
+        <div className="container">
+
+            <div className="row">
+
+                <div className="col-xs-4 col-md-4">
+
+                    <div className="Container-div">
+
+                        <div className="form-group blue-border-focus">
+                            <label>Duenno Asignacion</label>
+                            <input type="text" class="form-control" placeholder="Readonly input" readonly="readonly" />
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <label>Impacto incidencia</label>
+                            <input type="text" class="form-control" placeholder="Readonly input" readonly="readonly" />
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <label>Tecnologia incidencia</label>
+                            <input type="text" class="form-control" placeholder="Readonly input" readonly="readonly" />
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className="col-xs-4 col-md-4">
+
+                    <div className="Container-div">
+
+                        <div className="form-group blue-border-focus">
+                            <label>Tipo incidencia</label>
+                            <input type="text" class="form-control" placeholder="Readonly input" readonly="readonly" />
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <label>Fecha incidencia</label>
+                            <input type="text" class="form-control" placeholder="Readonly input" readonly="readonly" />
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+                <div className="col-xs-4 col-md-4">
+
+                    <div className="Container-div">
+
+                        <div className="form-group blue-border-focus">
+                            <label>Estado Actual</label>
+                            <input type="text" class="form-control" placeholder="Readonly input" readonly="readonly" />
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <label>Area incidencia</label>
+                            <input type="text" class="form-control" placeholder="Readonly input" readonly="readonly" />
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <label>Grado incidencia</label>
+                            <input type="text" class="form-control" placeholder="Readonly input" readonly="readonly" />
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
-  
-      </div>
-  
-  
+
+
     );
-  }
+}
 
 
-  export default InformacionIncidencia;
+export default InformacionIncidencia;
 
